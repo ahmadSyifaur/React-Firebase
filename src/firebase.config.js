67ed642e1b6151a6d7970dac103b4cb0a23a550f
AhmadSyifaur <ahmadsyifaur11@gmail.com>
2020-04-16 15:19:29 +0700

    cofig firebase , create login page .ext

---
+++ src/firebase.config.js
@@ -0,0 +1,9 @@
+export default{
+        apiKey: "AIzaSyBnhfJfFewbboIqUBWq4tlMtS16hlyK2AI",
+        authDomain: "react-firebase-modul-10.firebaseapp.com",
+        databaseURL: "https://react-firebase-modul-10.firebaseio.com",
+        projectId: "react-firebase-modul-10",
+        storageBucket: "react-firebase-modul-10.appspot.com",
+        messagingSenderId: "905258645038",
+        appId: "1:905258645038:web:3d1ba6e7b8a8a48b4091bf"
+}
